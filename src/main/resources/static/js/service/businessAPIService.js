d0e98f4cdbd23302171a236d5f032693b3c84a22
filipecsoares <filipecarlossoares@gmail.com>
2017-04-28 00:25:22 -0300

    pequenos ajustes

--- src/main/resources/static/js/service/businessAPIService.js
+++ src/main/resources/static/js/service/businessAPIService.js
@@ -1,14 +1,14 @@
 angular.module("agenda").service("businessAPI", function($http) {
 	this.getBusinesses = function(){
-		return $http.get("http://localhost:8080/business");
+		return $http.get("http://localhost:8090/business");
 	};
 	this.getBusiness = function(businessId){
-		return $http.get("http://localhost:8080/business/" + businessId);
+		return $http.get("http://localhost:8090/business/" + businessId);
 	};
 	this.saveBusiness = function(business){
-		return $http.post("http://localhost:8080/business", business);
+		return $http.post("http://localhost:8090/business", business);
 	};
 	this.deleteBusiness = function(businessId){
-		return $http.delete("http://localhost:8080/business/" + businessId);
+		return $http.delete("http://localhost:8090/business/" + businessId);
 	};
 });
--- src/main/resources/static/js/service/userAPIService.js
+++ src/main/resources/static/js/service/userAPIService.js
@@ -1,14 +1,14 @@
 angular.module("agenda").service("userAPI", function($http) {
 	this.getUsers = function(){
-		return $http.get("http://localhost:8080/user");
+		return $http.get("http://localhost:8090/user");
 	};
 	this.getUser = function(userId){
-		return $http.get("http://localhost:8080/user/" + userId);
+		return $http.get("http://localhost:8090/user/" + userId);
 	};
 	this.saveUser = function(user){
-		return $http.post("http://localhost:8080/user", user);
+		return $http.post("http://localhost:8090/user", user);
 	};
 	this.deleteUser = function(userId){
-		return $http.delete("http://localhost:8080/user/" + userId);
+		return $http.delete("http://localhost:8090/user/" + userId);
 	};
 });
